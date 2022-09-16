acd7312f29c722d699c41b80bdb43baf6964cf06
Fazekas László <fazekas@inf.u-szeged.hu>
2022-09-16 17:22:41 +0200

    create new structure

--- Steam Deck UI Patcher.py
+++ Steam Deck UI Patcher.py
@@ -1,19 +1,33 @@
 import os
-import os.path
-import time
+from os.path import join as os_join
+from os.path import sep
 from pathlib import Path
+from time import sleep
+from typing import Final
+
+STEAM_PKG_PATH: Final[Path] = Path("C:\Program Files (x86)\Steam\package")
+STEAM_BETA_PATH: Final[Path] = Path()
+
+def steamkill() -> int:
+    "Kills the steam task"
+    return os.system("taskkill /f /im steam.exe")
+
+def main() -> None:
+    ...
+
+if __name__ == "__main__":
+    main()
+
 # With love from czarro1337/bruhLNV! <3
 print("Checking if Steam directory on C:\ drive exists.")
-path = Path("C:\Program Files (x86)\Steam\package")
-beta = Path("C:\Program Files (x86)\Steam\package\beta")
-steamkill = lambda: os.system("taskkill /f /im steam.exe")
-if path.is_dir():
+# steamkill = lambda: os.system("taskkill /f /im steam.exe")
+if STEAM_PKG_PATH.is_dir():
     print("Steam directory was found.")
-    time.sleep(2)
+    sleep(2)
     print("Warning! Case sensitive.")
     confirm = input("By applying this patch you are installing the Steam Deck UI. Are you sure you want to install this? Y/N? >> ")
     if confirm == "Y":
-        os.chdir(path)
+        os.chdir(STEAM_PKG_PATH)
         print("Checking if the beta file exists")
         print("Applying steam patch.")
         print("Killing Steam proccess.")
@@ -21,18 +35,18 @@ if path.is_dir():
         f = open("beta", "w")
         f.write("steampal_stable_9a24a2bf68596b860cb6710d9ea307a76c29a04d")
         f.close()
-        time.sleep(0.64)
+        sleep(0.64)
         print("Written to beta file.")
-        time.sleep(0.53234)
+        sleep(0.53234)
         print("Done.")
         print("Script has applied the patch. Make sure you go to Steam's shortcut properties and on Target add -gamepadui.")
         print("Script will exit in 5 seconds.")
-        time.sleep(5)
+        sleep(5)
         exit()
     elif input == "N":
         print("Exiting.")
     exit()
 else:
     print("Steam directory was not found. If you have Steam on any other drive or partition change the location in path variable.")
-    time.sleep(3)
+    sleep(3)
     exit()
