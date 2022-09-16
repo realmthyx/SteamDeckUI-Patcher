0c77bde2251e6bda25f74e7e228572b0c4c272ba
Fazekas László <fazekas@inf.u-szeged.hu>
2022-09-16 17:42:31 +0200

    removed unnecessary sleep calls, minor refactor

--- Steam Deck UI Patcher.py
+++ Steam Deck UI Patcher.py
@@ -15,39 +15,39 @@ def steamkill() -> int:
     return os.system("taskkill /f /im steam.exe")
 
 def main() -> None:
+    """
+        :raises FileNotFoundError: when beta file was not found
+    """
     # With love from czarro1337/bruhLNV! <3
     print("Checking if Steam directory on C:\ drive exists.")
     if STEAM_PKG_PATH.is_dir():
         print("Steam directory was found.")
-        sleep(2)
-        print("Warning! Case sensitive.")
-        confirm = input("By applying this patch you are installing the Steam Deck UI. Are you sure you want to install this? Y/N? >> ")
-        if confirm == "Y":
+        install_confirm = input("By applying this patch you are installing the Steam Deck UI. Are you sure you want to install this? Y/N? >> ")
+
+        if install_confirm.upper() == "Y":
             os.chdir(STEAM_PKG_PATH)
 
-            print("Checking if the beta file exists")
-            print("Applying steam patch.")
+            print("Checking if the beta file exists: ", end = "")
+            if STEAM_BETA_PATH.exists():
+                print("OK")
+            else:
+                FileNotFoundError("beta file does not exist!")
+
             print("Killing Steam proccess.")
             steamkill()
 
+            print("Applying steam patch.")
             with open("beta", "w") as beta_file:
                 beta_file.write("steampal_stable_9a24a2bf68596b860cb6710d9ea307a76c29a04d")
 
-            sleep(0.64)
-            print("Written to beta file.")
-            sleep(0.53234)
-            print("Done.")
             print("Script has applied the patch. Make sure you go to Steam's shortcut properties and on Target add -gamepadui.")
             print("Script will exit in 5 seconds.")
-            sleep(5)
-            exit()
-        elif input == "N":
+        else:
             print("Exiting.")
-            exit()
     else:
         print("Steam directory was not found. If you have Steam on any other drive or partition change the location in path variable.")
-        sleep(3)
-        exit()
+
+    exit() # is this necessary on SD?
 
 
 if __name__ == "__main__":
