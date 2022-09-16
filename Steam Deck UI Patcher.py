23eec0464da6156b0b7df1e6c3fd8bb3d63c90b2
Fazekas László <fazekas@inf.u-szeged.hu>
2022-09-16 17:53:15 +0200

    increase cohesion, make paths a bit less platform dependent, remove unused imports

--- Steam Deck UI Patcher.py
+++ Steam Deck UI Patcher.py
@@ -1,25 +1,26 @@
 import os
-from os.path import join as os_join
 from os.path import sep
 from pathlib import Path
-from platform import platform
-from time import sleep
 from typing import Final
 
 # TODO: make this not platform sensitive
-STEAM_PKG_PATH: Final[Path] = Path("C:\Program Files (x86)\Steam\package")
-STEAM_BETA_PATH: Final[Path] = Path("C:\Program Files (x86)\Steam\package\beta")
+STEAM_PKG_PATH: Final[Path] = Path(sep.join(("C:", "Program Files (x86)", "Steam", "package")))
+STEAM_BETA_PATH: Final[Path] = Path(STEAM_PKG_PATH.joinpath("beta"))
 
 def steamkill() -> int:
     "Kills the steam task"
     return os.system("taskkill /f /im steam.exe")
 
+def steam_patch() -> None:
+    "Patches the beta file"
+    with open("beta", "w") as beta_file:
+        beta_file.write("steampal_stable_9a24a2bf68596b860cb6710d9ea307a76c29a04d")
+
+    print("Script has applied the patch. Make sure you go to Steam's shortcut properties and on Target add -gamepadui.")
+
 def main() -> None:
-    """
-        :raises FileNotFoundError: when beta file was not found
-    """
     # With love from czarro1337/bruhLNV! <3
-    print("Checking if Steam directory on C:\ drive exists.")
+    print("Checking if Steam directory on C: drive exists.")
     if STEAM_PKG_PATH.is_dir():
         print("Steam directory was found.")
         install_confirm = input("By applying this patch you are installing the Steam Deck UI. Are you sure you want to install this? Y/N? >> ")
@@ -31,17 +32,14 @@ def main() -> None:
             if STEAM_BETA_PATH.exists():
                 print("OK")
             else:
-                FileNotFoundError("beta file does not exist!")
+                print("FAIL")
+                print("The program will try to create it!")
 
             print("Killing Steam proccess.")
             steamkill()
 
             print("Applying steam patch.")
-            with open("beta", "w") as beta_file:
-                beta_file.write("steampal_stable_9a24a2bf68596b860cb6710d9ea307a76c29a04d")
-
-            print("Script has applied the patch. Make sure you go to Steam's shortcut properties and on Target add -gamepadui.")
-            print("Script will exit in 5 seconds.")
+            steam_patch()
         else:
             print("Exiting.")
     else:
