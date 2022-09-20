66a595846ea7b2f7c06fb2a0547106cf491c48ad
bruhLNV alt <111368720+czarro1337@users.noreply.github.com>
2022-09-20 20:18:37 +0200

    Linux version.
    Tested on Fedora 36 VM. This will work with every single linux distro I think. Have fun!

---
+++ Steam Deck UI Patcher Linux.py
@@ -0,0 +1,59 @@
+import os
+import time
+from os.path import sep
+from pathlib import Path, PurePosixPath
+from typing import Final, NoReturn
+
+# TODO: make this not platform sensitive
+# Thanks to flaszlo2000 for code refactoring, code is more readable!
+STEAM_PKG_PATH: Final[Path] = Path(sep.join((".steam", "steam", "package")))
+STEAM_BETA_PATH: Final[Path] = Path(STEAM_PKG_PATH.joinpath("beta"))
+
+def steamkill() -> int:
+    "Kills the steam task"
+    return os.system("killall steam")
+
+def steam_patch() -> None:
+    "Patches the beta file"
+    with open("beta", "w") as beta_file:
+        beta_file.write("steampal_stable_9a24a2bf68596b860cb6710d9ea307a76c29a04d")
+
+    print("Script has applied the patch. Make sure you go to your Terminal and type steam -gamepadui or make a bash script.")
+    time.sleep(3)
+
+def terminate(msg: str) -> NoReturn:
+    print(msg)
+    exit()
+
+def main() -> None:
+    # With love from czarro1337/bruhLNV! <3
+    print("Checking if Steam directory on your drive exists.")
+    if not STEAM_PKG_PATH.is_dir():
+        terminate("Steam directory was not found. Exiting.")
+
+    print("Steam directory was found.")
+    time.sleep(0.721534)
+    install_confirm = input("By applying this patch you are installing the Steam Deck UI. Are you sure you want to install this? Y/N? >> ")
+
+    if install_confirm.upper() != "Y":
+        terminate("Exiting.")
+
+    os.chdir(STEAM_PKG_PATH)
+
+    print("Checking if the beta file exists: ", end = "")
+    if STEAM_BETA_PATH.exists():
+        print("File exists.")
+        print("Overwriting.")
+    else:
+        print("File doesn't exist")
+        print("The program will try to create it!")
+
+    print("Killing Steam proccess.")
+    steamkill()
+
+    print("Applying steam patch.")
+    steam_patch()
+
+
+if __name__ == "__main__":
+    main()
